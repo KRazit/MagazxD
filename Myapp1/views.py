fc8ce036b2532116bb9329f915983575d9619103
KRazit <timurnyaa@gmail.com>
2023-02-03 18:53:38 +0300

    Ver1

--- Myapp1/views.py
+++ Myapp1/views.py
@@ -10,11 +10,11 @@ def MainPage(request):
     return render(request, 'MainPage.html', locals())
 
 def GetPerson(request):
+    if 
+        first_name1 = request.POST.get("first_name")
+        last_name1 = request.POST.get("last_name")
+        salary1 = request.POST.get("salary")
+        company1 = request.POST.get("company",)
 
-    first_name1 = request.POST.get("first_name")
-    last_name1 = request.POST.get("last_name")
-    salary1 = request.POST.get("salary")
-    company1 = request.POST.get("company",)
-
-    users_create = Person.objects.create(first_name = first_name1, last_name = last_name1, salary = salary1, company = company1)
+        users_create = Person.objects.create(first_name = first_name1, last_name = last_name1, salary = salary1, company = company1)
     return render(request, 'creater.html', locals())
